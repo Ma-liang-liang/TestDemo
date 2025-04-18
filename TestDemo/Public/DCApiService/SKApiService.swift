815006460a748a683898f8fdbd3b8e7a257297b8
马亮亮 <maliangliang@sdpjw.com>
2025-04-18 22:44:55 +0800

    网络库使用NSError纯传递错误 增加async, await 网络请求支持

--- TestDemo/Public/DCApiService/SKApiService.swift
+++ TestDemo/Public/DCApiService/SKApiService.swift
@@ -11,11 +11,11 @@ import Moya
 import SwifterSwift
 
 class SKApiService: NSObject {
-
-    typealias SuccessCallBack = ((String) -> Void)
-    typealias FailureCallBack = ((SKServiceError) -> Void)
     
-        
+    typealias SuccessCallBack = ((String) -> Void)
+    typealias FailureCallBack = ((NSError) -> Void)
+    
+    
     static func sendObjectRequest<T: SmartCodable>(apiProtocol: SKApiProtocol, modelType: T.Type ,desinatedPath: String? = "data", successCallBack: @escaping ((T?) -> ()), failureCallBack: @escaping FailureCallBack) {
         
         sendRequest(apiProtocol: apiProtocol, successCallBack: { json in
@@ -32,21 +32,21 @@ class SKApiService: NSObject {
         }, failureCallBack: failureCallBack)
     }
     
-   static func sendRequest(apiProtocol: SKApiProtocol, successCallBack: @escaping SuccessCallBack, failureCallBack: @escaping FailureCallBack) {
+    static func sendRequest(apiProtocol: SKApiProtocol, successCallBack: @escaping SuccessCallBack, failureCallBack: @escaping FailureCallBack) {
         
-       let params = SKApiConfig.commonParams + apiProtocol.parameters
-       let headers = SKApiConfig.commonHeaders + apiProtocol.headers
-              
-       let target = SKApiProtcolTarget(baseUrl: apiProtocol.baseUrlString,
-                                       path: apiProtocol.path,
-                                       method: apiProtocol.methodType,
-                                       requestType: apiProtocol.requestType,
-                                       headers: headers,
-                                       parameters: params,
-                                       contentType: apiProtocol.contentType,
-                                       multipartDatas: apiProtocol.multipartDatas,
-                                       downloadDestination: apiProtocol.downloadDestination)
-
+        let params = SKApiConfig.commonParams + apiProtocol.parameters
+        let headers = SKApiConfig.commonHeaders + apiProtocol.headers
+        
+        let target = SKApiProtcolTarget(baseUrl: apiProtocol.baseUrlString,
+                                        path: apiProtocol.path,
+                                        method: apiProtocol.methodType,
+                                        requestType: apiProtocol.requestType,
+                                        headers: headers,
+                                        parameters: params,
+                                        contentType: apiProtocol.contentType,
+                                        multipartDatas: apiProtocol.multipartDatas,
+                                        downloadDestination: apiProtocol.downloadDestination)
+        
         let provider = SKApiService.getProviderByTarget(target: target)
         let m_target = MultiTarget(target)
         ///获取调试信息
@@ -58,9 +58,9 @@ class SKApiService: NSObject {
                 let jsonStr = response.data.string(encoding: .utf8) ?? ""
                 
                 logInfo += "\n responseString = \(jsonStr)"
-
+                
                 if response.statusCode != DCHttpCode.successCode {
-                    let error = SKServiceError(code: "\(response.statusCode)", reason: jsonStr, api: target.path)
+                    let error = NSError(domain: "\(target.path)\n\(jsonStr)", code: response.statusCode)
                     failureCallBack(error)
                     return
                 }
@@ -78,8 +78,8 @@ class SKApiService: NSObject {
                 if code == "\(DCHttpCode.successCode)" {
                     successCallBack(jsonStr)
                 } else {
-                    let message = dict["message"] as? String
-                    let service_error = SKServiceError(code: "\(code)", reason: message ?? "", api: target.path)
+                    let message = (dict["message"] as? String) ?? ""
+                    let service_error = NSError(domain: "\(target.path)\n\(message)", code: response.statusCode)
                     failureCallBack(service_error)
                 }
             case .failure(let error):
@@ -90,7 +90,7 @@ class SKApiService: NSObject {
                     SKApiConfig.listenerCallBack?(error.errorCode)
                     return
                 }
-                let service_error = SKServiceError(code: "\(error.errorCode)", reason: error.errorDescription ?? "", api: target.path)
+                let service_error = NSError(domain: "\(target.path)\n\(String(describing: error.errorDescription))", code: error.errorCode)
                 failureCallBack(service_error)
             }
             ///判断是否需要log信息
@@ -101,12 +101,12 @@ class SKApiService: NSObject {
         }
     }
     
-    static func getProviderByTarget(target: TargetType) -> MoyaProvider<MultiTarget> {
+    private static func getProviderByTarget(target: TargetType) -> MoyaProvider<MultiTarget> {
         let provider = MoyaProvider<MultiTarget>(endpointClosure: { (target: MultiTarget) -> Endpoint in     // 设置header
             let url = target.baseURL.appendingPathComponent(target.path).absoluteString
             let headers:[String: String] = target.headers ?? [:]
             let endpoint = Endpoint(url: url, sampleResponseClosure: { () -> EndpointSampleResponse in
-                .networkResponse(200, target.sampleData)
+                    .networkResponse(200, target.sampleData)
             }, method: target.method, task: target.task, httpHeaderFields: headers)
             return endpoint
         }, requestClosure: { (point: Endpoint, closure: MoyaProvider<MultiTarget>.RequestResultClosure) in
@@ -119,8 +119,7 @@ class SKApiService: NSObject {
             }
         }, stubClosure: { (target: MultiTarget) -> StubBehavior in
             return .never
-        }, callbackQueue: DispatchQueue.main,
-           plugins: [NetworkActivityPlugin { (type, target) in
+        }, callbackQueue: DispatchQueue.main, plugins: [NetworkActivityPlugin { (type, target) in
             switch type {
             case .began:
                 break
@@ -130,7 +129,42 @@ class SKApiService: NSObject {
         }])
         return provider
     }
-
+    
 }
 
-
+extension SKApiService {
+    
+    static func sendObjectRequest<T: SmartCodable>(apiProtocol: SKApiProtocol, modelType: T.Type ,desinatedPath: String? = "data") async throws -> T? {
+        
+        try await withCheckedThrowingContinuation { continuation in
+            sendObjectRequest(apiProtocol: apiProtocol, modelType: modelType) { model in
+                continuation.resume(returning: model)
+            } failureCallBack: { error in
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+    
+    static func sendArrayRequest<T: SmartCodable>(apiProtocol: SKApiProtocol, modelType: T.Type, desinatedPath: String? = "data") async throws -> [T] {
+        
+        try await withCheckedThrowingContinuation { continuation in
+            sendArrayRequest(apiProtocol: apiProtocol, modelType: modelType) { models in
+                continuation.resume(returning: models)
+            } failureCallBack: { error in
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+    
+    static func sendRequest(apiProtocol: SKApiProtocol) async throws -> String {
+        
+        try await withCheckedThrowingContinuation { continuation in
+            sendRequest(apiProtocol: apiProtocol) { json in
+                continuation.resume(returning: json)
+            } failureCallBack: { error in
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+    
+}
