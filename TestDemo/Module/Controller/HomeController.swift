4fa72e1e12dcaa87695b8adb7c3b516b662e4524
maliangliang <1663466426@qq.com>
2025-07-10 23:38:56 +0800

    增加SwiftUI的弹出视图页面

--- TestDemo/Module/Controller/HomeController.swift
+++ TestDemo/Module/Controller/HomeController.swift
@@ -17,26 +17,58 @@ class HomeController: SKBaseController {
         
         view.backgroundColor = .white
         
-        view.addSubview(jumpBtn)
+        view.addSubviews {
+            jumpBtn
+            jumpBtn1
+        }
         
         jumpBtn.snp.makeConstraints { make in
             make.top.leading.equalToSuperview().offset(100)
             make.height.equalTo(36)
         }
-        
+                
+        jumpBtn1.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(100)
+            make.top.equalTo(jumpBtn.snp.bottom).offset(40)
+            make.height.equalTo(36)
+        }
      
         
     }
    
     @objc
     func onJumpClick(_ sender:UIButton) {
-        let vc = UIHostingController(rootView: MainTabBarControllerRepresentable())
-        present(vc, animated: true)
+        if sender == jumpBtn {
+           let vc = UIHostingController(rootView: MainTabBarControllerRepresentable())
+//            vc.modalPresentationStyle = .overFullScreen
+//            vc.modalTransitionStyle = .crossDissolve
+            present(vc, animated: true)
+        } else if sender == jumpBtn1 {
+            let page = ALSheetPage(onDismiss: {
+                
+            }, onPinkAreaTap: {
+                
+            })
+            let vc = UIHostingController(rootView: page)
+            vc.view.backgroundColor = .clear
+            vc.modalPresentationStyle = .overFullScreen
+            vc.modalTransitionStyle = .crossDissolve
+            present(vc, animated: true)
+        }
+       
     }
     
     lazy var jumpBtn: UIButton = {
         let button = UIButton()
         button.setTitle("  跳转  ", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(onJumpClick), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var jumpBtn1: UIButton = {
+        let button = UIButton()
+        button.setTitle("  跳转 present ", for: .normal)
         button.setTitleColor(.red, for: .normal)
         button.addTarget(self, action: #selector(onJumpClick), for: .touchUpInside)
         return button
